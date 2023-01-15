8609640bfed7db6c2712d11d9bc609d23fbdf37b
joao mastino <s.trinchero@gmail.com>
2023-01-15 16:40:10 +0100

    Aggiunto prime traduzioni e credit

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ import functions.functions as fc
 class OCR:
 
     def __init__(self):
-        #altera titulo da pagina
+        # titolo della pagina
         st.set_page_config(page_title="Python OCR")
-        #inicializa variveis
+        # inizializza variabili
         self.texto = ""
         self.analisar_texto = False
 
     def inicial(self):
-        #conteudo inicial da pagina
-        st.title("OCR Programa")
-        st.write("Optical Character Recognition (OCR) implementado com Python")
-        imagem = st.file_uploader("Selecione alguma imagem", type=["png","jpg"])
-        #se selecionar alguma imagem...
+        # contenuto iniziale della pagina
+        st.title("OCR per estrarre testi da immagini")
+        st.write("Forked by: https://github.com/guilhermedonizetti/OCR_Python")
+        st.write("Optical Character Recognition (OCR) in Python")
+        imagem = st.file_uploader("Seleziona un'immagine", type=["png","jpg"])
+        # se selezioni un'immagine
         if imagem:
             img = Image.open(imagem)
             st.image(img, width=350)
@@ -28,38 +29,40 @@ class OCR:
             self.texto = self.extrair_texto(img)
             st.write("{}".format(self.texto))
             
-            #Opcao de analisar texto
+            # Opzione analisi del testo
             self.analisar_texto = st.sidebar.checkbox("Analisar texto")
             if self.analisar_texto==True:
                 self.mostrar_analise()
     
     def extrair_texto(self, img):
-        #O comando que extrai o texto da imagem
+        # Comando che estrae testo dalle immagini con Tesseract
         texto = pytesseract.image_to_string(img, lang="ita")
         return texto
     
     def mostrar_analise(self):
         #busca CPF, datas e palavras boas e mas na extracao
+        # DA CAMBIARE: usare Spacy per analisi del testo
         cpf = fc.buscar_cpf(self.texto)
         datas = fc.buscar_data(self.texto)
         p_boas, percentual_bom = fc.buscar_palavras_boas(self.texto)
         p_mas, percentual_mau = fc.buscar_palavras_mas(self.texto)
         
         if cpf==None:
-            st.warning("Nenhum CPF encontrado.")
+            st.warning("Nessuna voce di autorità trovata.")
         else:
             cpf = fc.sumarizar_cpf(cpf)
-            st.success("CPF encontrado:")
+            st.success("Voci di autorità estratte:")
             st.write(cpf)
 
         if datas==None:
-            st.warning("Nenhuma data encontrada.")
+            st.warning("Nessuna data trovata.")
         else:
             datas = fc.sumarizar_datas(datas)
-            st.success("Datas encontradas:")
+            st.success("Date estratte:")
             st.write(datas)
         
         if p_boas==0:
+        # TO-DO: togliere?
             st.warning("Não identificado palavras de bem.")
         else:
             st.success("Palavras de bem:")
